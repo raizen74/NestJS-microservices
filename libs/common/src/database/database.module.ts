9e43f9152c1c50d545fd9cc23976818e4e3d8fa1
David Galera <raizen74@gmail.com>
2025-08-27 18:23:10 +0200

    Config and Database modules

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  imports: [MongooseModule.forRootAsync({  // allows to initialize the module on app startup and access its dependencies
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+        uri: configService.get("MONGODB_URI")
+    }),
+    inject: [ConfigService],
+  })]
+})
+export class DatabaseModule {}
